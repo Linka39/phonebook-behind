e3a0ec48ac1b2ab0b7b7fd09c2d995afc08eea6e
linka39 <1203440758@qq.com>
2019-12-20 15:49:45 +0800

    后端 mvn打包

--- src/test/java/com/linka39/PhonebookApplicationTests.java
+++ src/test/java/com/linka39/PhonebookApplicationTests.java
@@ -2,13 +2,16 @@ package com.linka39;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class PhonebookApplicationTests {
+public class PhonebookApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
